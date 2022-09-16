f677c6261ae2c45181e01bf7788ddf0845c49a2a
juicyaloe <mink4661@snu.ac.kr>
2022-09-17 07:44:38 +0900

    correct2

--- backend/models/user.js
+++ backend/models/user.js
@@ -19,7 +19,7 @@ module.exports = class User extends Sequelize.Model {
       },
     }, {
       sequelize,
-      timestamps: false,
+      timestamps: true,
       underscored: false,
       modelName: 'User',
       tableName: 'users',
