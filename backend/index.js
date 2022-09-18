93898725750eabb3767dd5b5c26c0741becc3784
juicyaloe <mink4661@snu.ac.kr>
2022-09-18 20:56:16 +0900

    require setting

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,52 @@
+// import package
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
 const morgan = require('morgan');
+const session = require('express-session');
+const dotenv = require('dotenv');
+dotenv.config();
 
+// import db
 const { sequelize } = require('./models');
 const User = require('./models/user');
 
+// import socket
 const webSocket = require('./socket');
 
+// app start
 const app = express();
 app.set('port', process.env.PORT || 8000);
 
+// db connect
 sequelize.sync({ force: false })
   .then(() => {
     console.log('db connect ok');
   })
   .catch((err) => {
+    console.log('db connect error');
     console.error(err);
   }
 );
 
+// initialize setting
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: false,
+//   secret: process.env.COOKIE_SECRET,
+//   cookie: {
+//     httpOnly: true,
+//     secure: false,
+//   },
+// }));
 
+// routes
 app.use('/api/', async (req, res) => {
     try {
         const users = await User.findAll({});
