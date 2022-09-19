65de36479404092e287f735f99bc8d8c1222ac9b
juicyaloe <mink4661@snu.ac.kr>
2022-09-19 14:01:31 +0900

    fix error

--- backend/index.js
+++ backend/index.js
@@ -66,8 +66,18 @@ app.use((req, res, next) => {
   next(error);
 });
 
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
 app.use((err, req, res, next) => {
-  res.send('error');
+  //console.error(err);
+  //res.locals.message = err.message;
+  //res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.send(err.message + err);
 });
 
 const server = app.listen(app.get('port'), () => {
